4b3d8ae7b1f94f2d2e2fc34674e1aea764e8ced3
catsmile100 <superteman12@gmail.com>
2023-12-20 10:25:37 +0700

    Add multiplication program

---
+++ pkgx/multiply.py
@@ -0,0 +1,7 @@
+# multiply.py
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = num1 * num2
+
+print(f"The result of the multiplication is: {result}")
